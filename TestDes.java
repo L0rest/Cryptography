a6051709fb20bdfc8ef5f9e7b30c60297c254698
L0rest <jawad.maache@gmail.com>
2023-09-13 14:18:17 +0200

    Tests added

--- TestDes.java
+++ TestDes.java
@@ -1,7 +1,12 @@
+import java.util.Arrays;
+
 public class TestDes {
 
     public static void main(String[] args) {
         Des des = new Des();
-        System.out.println(des);
+
+        System.out.println(Arrays.toString(des.stringToBits("Hello World")));
+
+        System.out.println(des.bitsToString(des.stringToBits("Hello World")));
     }
 }
