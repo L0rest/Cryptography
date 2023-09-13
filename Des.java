4997a87ba2325c69cfa1e3dcd7105f9d1e74568c
L0rest <jawad.maache@gmail.com>
2023-09-13 14:18:10 +0200

    Methods added

--- Des.java
+++ Des.java
@@ -1,3 +1,4 @@
+import java.math.BigInteger;
 import java.util.Random;
 
 public class Des {
@@ -12,6 +13,28 @@ public class Des {
         for (int i = 0; i < 64; i++) {
             this.masterKey[i] = rand.nextInt(2);
         }
+    }
+
+    public int[] stringToBits(String message) {
+        String bits = new BigInteger(message.getBytes()).toString(2);
+
+        int[] block = new int[bits.length()];
+
+        for (int i = 0; i < bits.length(); i++) {
+            block[i] = Integer.parseInt(String.valueOf(bits.charAt(i)));
+        }
+
+        return block;
+    }
+
+    public String bitsToString(int[] block) {
+        StringBuilder s = new StringBuilder();
+
+        for (int j : block) {
+            s.append(j);
+        }
+
+        return new String(new BigInteger(s.toString(), 2).toByteArray());
     }
 
     public String toString() {
