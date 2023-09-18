dece3e1fc825a0592db50603409f67a2c4dff20f
L0rest <jawad.maache@gmail.com>
2023-09-18 11:55:27 +0200

    Tests added

--- TestDes.java
+++ TestDes.java
@@ -5,8 +5,13 @@ public class TestDes {
     public static void main(String[] args) {
         Des des = new Des();
 
-        System.out.println(Arrays.toString(des.stringToBits("Hello World")));
+        System.out.println(des);
 
-        System.out.println(des.bitsToString(des.stringToBits("Hello World")));
+        int[] test = des.generePermutation(16);
+
+        System.out.println("test : " + Arrays.toString(test));
+        System.out.println("decoupage : " + Arrays.deepToString(des.decoupage(test, 4)));
+        System.out.println("recollage : " + Arrays.toString(des.recollage_bloc(des.decoupage(test, 4))));
+        System.out.println("decale : " + Arrays.toString(des.decale_gauche(test, 4)));
     }
 }
