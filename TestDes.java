6a6f8ca42cca20021cf9c2266b5c9e2e328f3466
L0rest <jawad.maache@gmail.com>
2023-10-16 20:24:14 +0200

    Tests added

--- TestDes.java
+++ TestDes.java
@@ -23,8 +23,23 @@ public class TestDes {
         int[] permutation1 = {2,4,6,0,1,3,5,7};
         Assert.assertEquals(Arrays.toString(new int[] {3,5,7,1,2,4,6,8}), Arrays.toString(des.permutation(permutation1, bloc1)));
 
-        int[] bloc2 = {1,2,3,4,5,6,7,8};
+    }
 
+    @Test
+    public void TestXor(){
+        Des des = new Des();
+
+        int[] bloc1 = {0,0,0,0,0,0,0,0};
+        int[] bloc2 = {1,1,1,1,1,1,1,1};
+        Assert.assertEquals(Arrays.toString(new int[] {1,1,1,1,1,1,1,1}), Arrays.toString(des.xor(bloc1, bloc2)));
+
+        int[] bloc3 = {1,1,1,1,1,1,1,1};
+        int[] bloc4 = {1,1,1,1,1,1,1,1};
+        Assert.assertEquals(Arrays.toString(new int[] {0,0,0,0,0,0,0,0}), Arrays.toString(des.xor(bloc3, bloc4)));
+
+        int[] bloc5 = {1,0,1,1,0,1,1,0};
+        int[] bloc6 = {0,1,1,0,1,0,1,1};
+        Assert.assertEquals(Arrays.toString(new int[] {1,1,0,1,1,1,0,1}), Arrays.toString(des.xor(bloc5, bloc6)));
     }
 
 }
